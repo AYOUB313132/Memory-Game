7729ac047094b98198e310b4c75deb35856b1940
AYOUB313132 <46676570+AYOUB313132@users.noreply.github.com>
2022-06-04 02:52:37 +0200

    add sound

--- scripte/main.js
+++ scripte/main.js
@@ -1,64 +1,26 @@
 /* =========== Array Images =========== */
 let EmojiImg = [
-        "image/logo-1.png",
-        "image/logo-2.png",
-        "image/logo-3.png",
-        "image/logo-4.png",
-        "image/logo-5.png",
-        "image/logo-6.png",
-        "image/logo-7.png",
-        "image/logo-8.png",
-        "image/logo-9.png",
-        "image/logo-10.png",
-        "image/logo-1.png",
-        "image/logo-2.png",
-        "image/logo-3.png",
-        "image/logo-4.png",
-        "image/logo-5.png",
-        "image/logo-6.png",
-        "image/logo-7.png",
-        "image/logo-8.png",
-        "image/logo-9.png",
-        "image/logo-10.png",
-    ]
-    // shuffle Array 
-    // let gameBlock = document.getElementById('images').children;
-
-//         let otro = Array.from(gameBlock)
-
-//         // let one = [...Array(gameBlock.length).keys()]
-//         // let one = Array.from(Array(gameBlock.length).keys())
-//         let one = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-//         console.log(one)
-//         shuffle(one)
-//         console.log(one)
-
-//         function shuffle(arr) {
-//             let current = arr.length,
-//                 random,
-//                 temporal;
-//             while (current > 0) {
-//                 random = Math.floor(Math.random() * current);
-//                 current--;
-//                 temporal = arr[current]
-//                 arr[current] = arr[random]
-//                 arr[random] = temporal
-
-//             }
-
-//             return arr;
-
-//         }
-// getShuffledArr = arr => {
-//     const newArr = arr.slice()
-//     console.log(newArr)
-//     for (let i = newArr.length - 1; i > 0; i--) {
-//         const rand = Math.floor(Math.random() * (i + 1));
-//         [newArr[i], newArr[rand]] = [newArr[rand], newArr[i]];
-//     }
-//     return newArr
-// };
-// let finaleImage = getShuffledArr(img)
+    "image/logo-1.png",
+    "image/logo-2.png",
+    "image/logo-3.png",
+    "image/logo-4.png",
+    "image/logo-5.png",
+    "image/logo-6.png",
+    "image/logo-7.png",
+    "image/logo-8.png",
+    "image/logo-9.png",
+    "image/logo-10.png",
+    "image/logo-1.png",
+    "image/logo-2.png",
+    "image/logo-3.png",
+    "image/logo-4.png",
+    "image/logo-5.png",
+    "image/logo-6.png",
+    "image/logo-7.png",
+    "image/logo-8.png",
+    "image/logo-9.png",
+    "image/logo-10.png",
+];
 
 function customShuffle(x) {
     var y = x.slice(1, x.length - 1);
@@ -71,13 +33,13 @@ function customShuffle(x) {
     }
     return [x[0]].concat(y).concat(x[x.length - 1]);
 }
-
 let emoji = customShuffle(EmojiImg)
 
 $(document).ready(() => {
-    $('.level-1').click(function() {
+    $('#level-1').click(function() {
+
         $('.text').remove();
-        $('#game-blocks').addClass('grid')
+        $('#game-blocks').addClass('flex')
         for (let i = 0; i < emoji.length; i++) {
             $('#game-blocks').append('<div class="game-block hide"><div class="face front">?</div><div class="face back"><img id="back" src="" alt=""></div></div>')
         }
@@ -88,77 +50,50 @@ $(document).ready(() => {
             $(this).parents('.game-block').attr('data-tech', imgSrc.slice(6, -4));
         })
         $('#game-blocks .game-block').fadeIn(2000)
-
+        const audio = new Audio("/sound/mixkit-fun-times-7.mp3");
+        audio.play();
 
         let gameBlocks = $('#game-blocks').children()
-        gameBlocks.each(function() {
-            $(this).on('click', function() {
+        gameBlocks.each(function(e) {
+            $(this).on('click', function(e) {
                 $(this).addClass('flipped')
-                let classChecked = $(this).hasClass('flipped')
+                var classList = $('.flipped')
 
-            })
-
-        })
-        $('#game-blocks').children().each(function() {
-            let classChecked = $(this).hasClass('flipped')
-            $(this).filter(function() {
-                $(this).on('click', function() {
-                    if ($(this).hasClass('flipped')) {
-                        console.log($(this).length)
-                    } else {
-                        console.log('no')
-                    }
-                })
-
+                chekblocks(classList)
             })
         })
-
-
-
-
-
-
-    });
-
-
-
-
-
-
-
-
-
-
-
+    })
 })
 
+function chekblocks(classList) {
+    if (!classList || classList.length < 2) {
+        return;
+    } else if (classList && classList.length == 2) {
 
+        var first = classList.first().get(0);
+        var second = classList.last().get(0);
+        console.log(first, second)
+        if (first && second && first.dataset.tech === second.dataset.tech) {
+            //set equal class
+            first.classList.add('egual');
+            second.classList.add('egual');
+            //remove hide class 
+            //first.classList.remove('hide');
+            //second.classList.remove('hide');
+        }
+    }
+    resetClass()
+}
 
+function resetClass() {
+    setTimeout(function() {
+        //jquery
+        $('.game-block.flipped').removeClass('flipped');
+        //purejs 
+        //Array.from(document.querySelectorAll('.game-block.flipped')).forEach((el) => el.classList.remove('flipped'));
+    }, 1000)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
 
 
 
